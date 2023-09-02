0499645e896d8d7193e391800cee4d035959b737
Fabio <fabiosmedeiros@gmail.com>
2023-09-02 20:32:46 -0300

    Commit inicial.

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers; // Não esquecer de sempre ajustar o namespace
+
+// ControllerBase é a classe base quando trabalhamos com API
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+
+    private readonly DataContext _context;
+    public UsersController(DataContext context)
+    {
+        _context = context;// Isso vai ser injetado quando um UsersController for criado
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers ()
+    {
+        var users = _context.Users.ToListAsync();
+
+        return await users;
+    }
+
+    [HttpGet("{id}")] // /api/users/2
+    public async Task<ActionResult<AppUser>> GetUser (int id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
+
+}
